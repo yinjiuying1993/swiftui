4ed043753b89e33bf1c4fa6eb5992437573312f1
jiuying.yin <jiuying.yin@gaea.com>
2022-03-16 10:06:14 +0800

    Initial Commit

---
+++ oa-ios-new/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  oa-ios-new
+//
+//  Created by yinjiuying on 2022/3/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ oa-ios-new/oa_ios_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  oa_ios_newApp.swift
+//  oa-ios-new
+//
+//  Created by yinjiuying on 2022/3/16.
+//
+
+import SwiftUI
+
+@main
+struct oa_ios_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
